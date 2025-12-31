61f2f57148185827c8856d0f0ba9b2df728670b0
SamuelNava18 <brayan.briones@alumnos.udg.mx>
2025-12-30 18:45:47 -0600

    AxiosTest

---
+++ JavascriptEssentials/axiosTest/axiosTest.js
@@ -0,0 +1,21 @@
+// Import Axios
+const axios = require('axios');
+// Make a GET request
+// axios.get('https://jsonplaceholder.typicode.com/posts')
+//     .then(response => {
+//         // Handle successful response
+//         console.log('Response:', response.data);
+//     })
+//     .catch(error => {
+//         // Handle error
+//         console.error('Error:', error);
+//     });
+async function fetchData() {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        console.log('Response:', response.data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+fetchData();
